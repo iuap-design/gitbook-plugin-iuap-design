6eb2a3b993e60e69f9ddad005184f8bf3651d027
liuyk <liuyk@yonyou.com>
2016-06-14 17:02:07 +0800

    创建插件

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    website: {
+        assets: './book',
+        js: [
+            'iuap-desigin.js',
+            'expandable-chapters.js',
+        ],
+        css: [
+            'iuap-desigin.css',
+            'expandable-chapters.css',
+        ]
+    }
+};
