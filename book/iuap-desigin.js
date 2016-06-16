b8e017934e9c1d112a22dd615137ffe463fee640
liuyk <liuyk@yonyou.com>
2016-06-16 15:48:48 +0800

    修改点击目录之后空白的问题

--- book/iuap-desigin.js
+++ book/iuap-desigin.js
@@ -25,7 +25,8 @@ require(['gitbook', 'jQuery', 'lodash'], function (gitbook, $, _) {
 				]
 				var bannerHTML = banner.join('\r\n');
 				data = data + bannerHTML;
-
+				$('.navbar').remove();
+				$('.banner').remove();
 				$(document.body).prepend(data)
 			}
 		})
@@ -37,6 +38,7 @@ require(['gitbook', 'jQuery', 'lodash'], function (gitbook, $, _) {
 			async: false,
 			success: function(data) {
 				data = data.replace(/dist/g,'../..');
+				$('.footer').remove();
 				$(document.body).append(data)
 			}
 		})
@@ -44,11 +46,11 @@ require(['gitbook', 'jQuery', 'lodash'], function (gitbook, $, _) {
 
 		var $body = $('body');
 		// 文档部分
-		var $book = $('.book');
+		var $book = $('.book');  
 		// 文档左侧目录
-		var $summary = $('.book-summary');
+		var $summary = $('.book-summary'); 
 		// 文档右侧主体
-		var $bookBody = $('.book-body');
+		var $bookBody = $('.book-body'); 
 		var $container = $('<div class="container"></div>');
 		var $containerDiv = $('<div class="container-div"></div>');
 		$book.append($container);
@@ -56,21 +58,21 @@ require(['gitbook', 'jQuery', 'lodash'], function (gitbook, $, _) {
 		$containerDiv.append($summary);
 		$containerDiv.append($bookBody);
 
-		// 左侧目录修改
+		// 左侧目录修改 
 		var $summaryUl = $('ul',$summary);
 	 	var $firstLi = $('li:first',$summaryUl);
 	 	// 去掉介绍
-		$firstLi.remove();
+		$firstLi.remove(); 
 		var $dividerLi = $('.divider',$summaryUl);
 		var $dividerLiNext = $('.divider + li',$summaryUl);
 		// 删除下面横线及之后的li
-		$dividerLi.remove();
+		$dividerLi.remove(); 
 		$dividerLiNext.remove();
 
-		var $summaryAB = $('a b',$summary);
+		var $summaryAB = $('a b',$summary); 
 		$summaryAB.remove();// 去掉目录的编号
 
-		// 右侧主体修改
+		// 右侧主体修改 
 
 		var $bookHeadr = $('.book-header',$bookBody);
 		$bookHeadr.remove();
@@ -82,30 +84,50 @@ require(['gitbook', 'jQuery', 'lodash'], function (gitbook, $, _) {
 		$pageWrapper.append($prevA);
 		$pageWrapper.append($nextA);
 
-		// 主体引入uui内容
+		// 主体引入uui内容 
 		var ctx = 'http://design.yyuap.com/static/uui-original/1.0.1';
-		var tpl = [
-			'<link rel="stylesheet" href="'+ ctx +'/css/font-awesome.css">',
-	       	'<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/u.css">',
-	       	'<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/u-extend.css">',
-		    '<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/tree.css">',
-	        '<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/grid.css">',
-	        '<script src="http://design.yyuap.com/static/jquery/jquery-1.11.2.js"></script>',
-	    	'<script src="http://design.yyuap.com/static/knockout/knockout-3.2.0.debug.js"></script>',
-	        '<script src="'+ ctx +'/js/u-polyfill.js"></script>',
-	        '<script src="'+ ctx +'/js/u.js"></script>',
-	        '<script src="'+ ctx +'/js/u-tree.js"></script>',
-	        '<script src="'+ ctx +'/js/u-grid.js"></script>',
-        ]
-        var tplStr = tpl.join('\r\n');
+		// var tpl = [
+		// 	'<link rel="stylesheet" href="'+ ctx +'/fonts/font-awesome/css/font-awesome.css">',
+	 //       	'<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/u.css">',
+	 //       	'<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/u-extend.css">',
+		//     '<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/tree.css">',
+	 //        '<link rel="stylesheet" type="text/css" href="'+ ctx +'/css/grid.css">',
+	 //        '<script src="http://design.yyuap.com/static/jquery/jquery-1.11.2.js"></script>',
+	 //    	'<script src="http://design.yyuap.com/static/knockout/knockout-3.2.0.debug.js"></script>',
+	 //        '<script src="'+ ctx +'/js/u-polyfill.js"></script>',
+	 //        '<script src="'+ ctx +'/js/u.js"></script>',
+	 //        '<script src="'+ ctx +'/js/u-tree.js"></script>',
+	 //        '<script src="'+ ctx +'/js/u-grid.js"></script>',
+  //       ]
+        // var tplStr = tpl.join('\r\n');
+        var linkArray=['/fonts/font-awesome/css/font-awesome.css','/css/u.css','/css/u-extend.css','/css/tree.css','/css/grid.css'];
+        var scriptArray=['/js/u-polyfill.js','/js/u.js',/*'/js/u-tree.js','/js/u-grid.js'*/];
+      	for(var i=0;i<linkArray.length;i++){
+      		var link = document.createElement('link');
+      		link.rel='stylesheet';
+      		link.href=ctx+linkArray[i];
+      		 $('head').append(link);
+      	}
+        var jquerySrc= document.createElement('script');
+        jquerySrc.scr = 'http://design.yyuap.com/static/jquery/jquery-1.11.2.js';
+        $('head').append(jquerySrc);
 
-        document.write(tplStr);
+        var koSrc= document.createElement('script');
+        koSrc.scr = 'http://design.yyuap.com/static/knockout/knockout-3.2.0.debug.js';
+        $('head').append(koSrc);
+        var tim = 1000;
+        for(var j=0;j<scriptArray.length;j++){
+      		var script = document.createElement('script');
+      		script.src=ctx+scriptArray[j];
+  			$('head').append(script);
+      	}
+        // document.write(tplStr);
         var $html = $('html');
         $html.css('font-size','62.5%');
 
 
         $('body').css('display','block');
-
+        
         function bodyScrollFun(){
         	var st = document.body.scrollTop || document.documentElement.scrollTop;
         	var t = 215 - st;
@@ -121,6 +143,7 @@ require(['gitbook', 'jQuery', 'lodash'], function (gitbook, $, _) {
         function bodyScroll(){
         	document.body.onscroll = bodyScrollFun;
         }
+        document.body.scrollTop = 0;
         setInterval(bodyScroll,100)
         bodyScrollFun();
 
